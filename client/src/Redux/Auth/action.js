6491e9a92e68d6ec84c96400812558f0ca5e0c3e
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2023-01-28 22:26:07 +0530

    Auth reducer added

---
+++ client/src/Redux/Auth/action.js
@@ -0,0 +1,78 @@
+import axios from "axios"
+import * as types from "./actionTypes"
+import { dataUrl } from "../../Utils/AllUrls";
+import { toastHandlerF, toastHandlerS } from "../../Utils/toast";
+
+
+
+const registerUser = (payload, toast, navigate) => (dispatch) => {
+    try {
+        dispatch({ type: types.USER_REGISTRATION_REQUEST })
+        /* createUserWithEmailAndPassword(auth, payload.email, payload.password).then((res)=>{ */
+            return axios.post(`${dataUrl}/users/signup`, payload, {withCredentials:true}).then((res2) => {
+                console.log(res2)
+                dispatch({ type: types.USER_REGISTRATION_SUCCESS })
+                toastHandlerS(res2.data, toast, "Signup")
+                navigate("/login")
+            }).catch((error) => { 
+                console.log(error.code) 
+                toastHandlerF(error, toast)     
+                dispatch({ type: types.USER_REGISTRATION_FAILURE})
+            })
+        /* }).catch((er)=>{
+            console.log(er.message)
+            dispatch({ type: types.USER_REGISTRATION_FAILURE})
+            toastHandlerFire(er, toast)
+        }) */
+
+    } catch (error) {
+        console.log(error)
+        dispatch({ type: types.USER_REGISTRATION_FAILURE})
+        toastHandlerF(error, toast)
+    }
+    
+}
+
+const loginUser = (payload, toast, navigate) => (dispatch) => {
+    try {
+        dispatch({type: types.USER_LOGIN_REQUEST})
+        return axios.post(`${dataUrl}/users/login`, payload, {withCredentials:true}).then((res2) => {
+            console.log(res2)
+            dispatch({ type: types.USER_LOGIN_SUCCESS, payload: res2.data })
+            toastHandlerS(res2.data, toast, "Login")
+            navigate("/")
+    }).catch((error) => { 
+        console.log(error) 
+        toastHandlerF(error, toast)     
+        dispatch({ type: types.USER_LOGIN_FAILURE})
+    })
+    } catch (error) {
+        console.log(error.message)
+        dispatch({ type: types.USER_LOGIN_FAILURE})
+        toastHandlerF(error, toast) 
+    }
+}
+
+const logOut = (payload, toast, navigate) => (dispatch) => {
+    try {
+        dispatch({type: types.USER_LOGOUT_REQUEST})
+        return axios.post(`${dataUrl}/users/logout/${payload.email}`, payload, {withCredentials:true}).then((res2) => {
+        toastHandlerS(res2.data, toast)
+        console.log(res2)
+        dispatch({ type: types.USER_LOGOUT_SUCCESS })
+        navigate("/")
+    }).catch((error) => { 
+        console.log(error) 
+        toastHandlerF(error, toast)     
+        dispatch({ type: types.USER_LOGOUT_FAILURE})
+    })
+    } catch (error) {
+        console.log(error.message)
+        dispatch({ type: types.USER_LOGOUT_FAILURE})
+        toastHandlerF(error, toast) 
+    }
+}
+
+
+
+export { registerUser, loginUser, logOut }
---
+++ client/src/Redux/Auth/reducer.js
@@ -0,0 +1,41 @@
+import * as types from "./actionTypes";
+const initData = {
+ 
+}
+
+const initialState = {
+  isAuth: false,
+  isLoading: false,
+  isError: false,
+  userData: initData,
+};
+
+const AuthReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case types.USER_REGISTRATION_REQUEST:
+      return { ...state, isLoading: true, isError: false  };
+    case types.USER_REGISTRATION_SUCCESS:
+      return { ...state, isLoading: false,isError: false  };
+    case types.USER_REGISTRATION_FAILURE:
+      return { ...state, isLoading: false, isError: true };
+    case types.USER_LOGIN_REQUEST:
+      return { ...state, isLoading: true, isError: false };
+    case types.USER_LOGIN_SUCCESS:
+      return { ...state, isLoading: false, userData:payload, isError: false, isAuth: true  };
+    case "savedata":
+      return { ...state, isLoading: false, userData:payload, isError: false, isAuth: true  };
+    case types.USER_LOGIN_FAILURE:
+      return { ...state, isLoading: false, isError: true };
+    case types.USER_LOGOUT_REQUEST:
+      return { ...state, isLoading: true, isAuth: true };
+    case types.USER_LOGOUT_SUCCESS:
+      return { ...state, isLoading: false, userData:initData, isAuth: false, isError: false};
+    case types.USER_LOGOUT_FAILURE:
+      return { ...state, isLoading: false, isError: true };
+    default:
+      return state;
+  }
+};
+export default AuthReducer ;
