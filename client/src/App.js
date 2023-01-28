56966d1eab4b2e5cad756535d80d56570c86eab4
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2023-01-28 23:41:55 +0530

    deployment testing initiated

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import { Box } from '@chakra-ui/react';
 import './App.css';
 import AllRoutes from './Routes/AllRoutes';
+import axios from "axios"
+import { dataUrl } from './Utils/AllUrls';
 
 function App() {
+  axios.get(`${dataUrl}`).then((res)=>console.log(res)).catch((err)=>console.log(err.message))
   return (
     <Box className="App">
           <AllRoutes/>
