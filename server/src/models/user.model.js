8a33dad3eba15be2d93aad3aa0d196f652324db7
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2023-01-28 23:36:01 +0530

    both sides are ready for deployment

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,8 +1,8 @@
-const mongoose =require("mongoose")
+const mongoose = require("mongoose")
 
 
 
-const userSchema=new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     email:{type:String,required:true,unique:true},
     password:{type:String,required:true},
     role:{type:String,enum:["user","admin","CEO"],default:"user"},
