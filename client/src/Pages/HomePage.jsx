f1fd7fe44c4db1742fc65f09c995f2a7b36f958f
Sujeet Kumar <sujeetkumaryadav202020@gmail.com>
2023-01-29 15:07:46 +0530

    second test for cockies

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import { dataUrl } from '../Utils/AllUrls'
 import axios from "axios"
+import {Navigate} from "react-router-dom"
 
 const HomePage = () => {
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+  };
+  
+  function success(pos) {
+    const crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+  
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+  
+  navigator.geolocation.getCurrentPosition(success, error, options);
+  
+  if(navigator.cookieEnabled){
+    console.log(navigator.cookieEnabled)
+  } else {
+    return <Navigate to="/sd" />
+  }
   const doIt1 = async () => {
     axios.post(`${dataUrl}/users/login`,{email:"sujeet@gmail.com",password:"sujeet8"},{withCredentials:true}).then((res)=>console.log(res)).catch((err)=>console.log(err))
 
